eded0d48a4f08673d066b1594e0003ca71961a44
MauroPalgiLS <108412978+MauroPalgiLS@users.noreply.github.com>
2024-06-15 22:44:55 -0300

    electron with react

--- frontend/src/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("api", {
-  processPdf: (filePath) => ipcRenderer.invoke("process-pdf", filePath),
-});
